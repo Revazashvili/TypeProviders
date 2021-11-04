6203a48ac32a5299012f2e116676770129043fd1
Revazashvili <levanrevazashvili@gmail.com>
2021-11-04 21:15:33 +0400

    add project

---
+++ src/TypeProviders/Json.fs
@@ -0,0 +1,12 @@
+﻿open FSharp.Data
+
+[<Literal>]
+let url = "https://jsonplaceholder.typicode.com/users"
+type Users = JsonProvider<url>
+
+[<EntryPoint>]
+let main args =
+    Users.Load(url)
+    |> Seq.take 1
+    |> Seq.iter (fun user -> printfn $"User: {user}")
+    0
